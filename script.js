b34306ecad3838c2942ca9328375e0fd3d2fa280
Daniel00-Codes <Danielcrusoe00@gmail.com>
2022-06-23 22:54:57 +0100

    Commit1

---
+++ script.js
@@ -0,0 +1,18 @@
+var num1 = parseInt ( prompt("Number1; "))
+var opr = prompt ("Enter your operation : *,  +,  -,  /")
+var num2 = parseInt ( prompt("Number2; "))
+
+if (opr == "+")
+{
+  alert(num1 + num2)
+} else if (opr == "-")
+{
+  alert(num1 - num2)
+} else if (opr == "*")
+{
+  alert(num1 * num2)
+} else if (opr == "/")
+{
+  alert(num1 / num2)
+
+}
